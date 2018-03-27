8d7531ad4361ec68c1b975093a6ae3a8aa58fb98
Thomas Bonk <tbo@cybo.biz>
2018-03-28 00:16:53 +0200

    Offer simplified algolia search

--- handler.ts
+++ handler.ts
@@ -1,11 +1,40 @@
-module.exports.search = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    })
-  };
+const querystring = require('querystring');
+const fetch = require('node-fetch');
+const {stringify, parse} = require('qs');
 
-  callback(null, response);
+const endpoint = 'search-rebelle-de-of2zvlq5mmhvlk7hnlmgoaxkn4.eu-central-1.cloudsearch.amazonaws.com';
+const apiVersion = '2013-01-01';
+const url = `http://${endpoint}/${apiVersion}/search?`;
+
+const parseArray = field => (typeof field === 'string' && field.length) ? JSON.parse(field) : [];
+
+const conjunctFilters = (filters: string[]) => {
+  const filterString = filters
+    .map(item => item.split(':'))
+    .map(([key, value]) => `${key}:'${value}'`)
+    .join(' ');
+  return filters.length < 2 ? filterString : `(and ${filterString})`;
+}
+const getOptions = ({query, facetFilters, hitsPerPage = 100, page = 0, facets, disjunctiveFacets}) => ({
+  q: query,
+  facetFilters: parseArray(facetFilters),
+  size: hitsPerPage,
+  start: page * hitsPerPage,
+  facets,
+  disjunctiveFacets: parseArray(disjunctiveFacets)
+});
+
+const getQueryParams = ({facetFilters, facets, ...options}) => ({
+  ...options,
+  ...(facetFilters ? {fq: conjunctFilters(facetFilters), ...options} : {}),
+  ...parseArray(facets).reduce((prev, facet) => ({...prev, [`facet.${facet}`]: '{}'}), {})
+});
+
+module.exports.search = (event, _, callback) => {
+  const params = getQueryParams(getOptions(event.queryStringParameters));
+  fetch(url + querystring.stringify(params))
+    .then(result => result.text())
+    .then(body => {
+      callback(null, {statusCode: 200, body});
+    });
 };
